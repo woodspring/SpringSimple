892cc8bd2d5a187771393a8bc2e00ec25975c5cb
woodspring <woodspring.toronto@gmail.com>
2020-06-09 00:24:35 -0400

    1st commit

---
+++ src/main/java/woodspring/springsimple/SpringSimpleApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springsimple/SpringSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
